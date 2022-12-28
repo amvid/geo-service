9d8d0ea442b782bc94dd1546aa2f76c9f86c2dd3
Vadims Filipovskis <vadim.filipovsky@gmail.com>
2022-12-28 22:39:33 +0200

    feat: state response

--- src/State/Controller/Response/StateResponse.php
+++ src/State/Controller/Response/StateResponse.php
@@ -4,7 +4,24 @@ declare(strict_types=1);
 
 namespace App\State\Controller\Response;
 
+use App\Country\Controller\Response\CountryResponse;
+use App\State\Entity\State;
+use Ramsey\Uuid\UuidInterface;
+
 class StateResponse
 {
+    public UuidInterface $id;
+    public string $title;
+    public string $code;
+    public ?string $type;
+    public CountryResponse $country;
 
+    public function __construct(State $state)
+    {
+        $this->id = $state->getId();
+        $this->title = $state->getTitle();
+        $this->code = $state->getCode();
+        $this->type = $state->getType();
+        $this->country = new CountryResponse($state->getCountry());
+    }
 }
