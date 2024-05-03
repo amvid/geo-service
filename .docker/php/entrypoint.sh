06a88a0b7785c3a406d5683f6287cd84e1d8270b
Vadims <fvdev9@gmail.com>
2024-05-03 10:48:32 +0200

    dependencies upgrade
    - upgraded composer dependecies
    = upgraded roadrunner version to v2024.1.1

--- .docker/php/entrypoint.sh
+++ .docker/php/entrypoint.sh
@@ -4,13 +4,13 @@ if [ -d "vendor" ]; then
 	echo "Vendor folder exists. Skipping 'composer install'."
 else
 	composer install
+	bin/console assets:install
 fi
 
 if [ -f "bin/rr" ]; then
 	echo "RR binary exists. Skipping 'vendor/bin/rr get-binary'."
 else
-	bin/console assets:install
-	vendor/bin/rr get-binary -f v2024.1.0 --location bin
+	vendor/bin/rr get-binary -f v2024.1.1 --location bin
 	chmod +x bin/rr
 fi
 
