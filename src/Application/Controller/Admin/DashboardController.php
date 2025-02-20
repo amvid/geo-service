b7c2f4c15eea487c72a1277d5b6ef0d0a8798d21
Vadims <fvdev9@gmail.com>
2025-02-20 16:44:22 +0100

    chore(dashboard): removed unneeded btn

--- src/Application/Controller/Admin/DashboardController.php
+++ src/Application/Controller/Admin/DashboardController.php
@@ -55,7 +55,6 @@ class DashboardController extends AbstractDashboardController
 
     public function configureMenuItems(): iterable
     {
-        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
         yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
         yield MenuItem::linkToCrud('Currencies', 'fa fa-dollar', Currency::class);
         yield MenuItem::linkToCrud('Timezones', 'fa fa-clock', Timezone::class);
